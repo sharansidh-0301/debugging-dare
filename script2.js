a743e259849f13d9d5c58dbaef77ad4e72ab42a1
GAYATHRI1006 <gayathrigaya3ece@gmail.com>
2025-01-27 22:09:42 +0530

    Initial changes

---
+++ script2.js
@@ -0,0 +1,4 @@
+document.querySelector('.hamburger-menu').addEventListener('click', () => {
+    const navbarList = document.querySelector('.navbar-list');
+    navbarList.classList.toggle('open');
+  });
