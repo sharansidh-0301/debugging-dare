09090afce0a07c2f078c7598349705e67e7ce770
Subeshan2 <nsubeshan22@gmail.com>
2025-01-29 07:53:58 +0530

    ide flow of action completed

---
+++ easy.js
@@ -0,0 +1,4 @@
+const question_pop=document.querySelector(".question-pop"), dare_btn=document.querySelector(".dare-btn")
+dare_btn.addEventListener("click",()=>{
+    question_pop.style.visibility="visible"
+})
--- script.js
+++ script.js
@@ -7,8 +7,3 @@ close_login_page.addEventListener("click",(event)=>{
     event.preventDefault()
     login_pop.style.display="none"
 })
-let easy_question=document.querySelector(".easy_question"),intermediate_question=document.querySelector(".intermediate-question"),hard_question=document.querySelector(".hard-question")
-let anime_1=document.querySelector("#anime-1"),anime_2=document.querySelector("#anime-2"),anime_3=document.querySelector("#anime-3")
-easy_question.addEventListener("click",()=>{
-    anime_1.style="left:250px;"
-})
